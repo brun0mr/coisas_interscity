ac89ebda96ed6b05b596799702569683e991e3b1
brun0mr <bruno.rotondaro@unifesp.br>
2020-05-23 17:24:40 -0300

    teste.py funcionando

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 
 
-'''
+
 inf = '{"data": {"environment_monitoring": [{"teste": 11,"timestamp": "2020-05-23T13:52:25.428Z"}]}}'
 headers = {'content-Type': 'application/json'}
 
@@ -10,9 +10,4 @@ headers = {'content-Type': 'application/json'}
 r = requests.post("http://34.95.169.45:8000/adaptor/resources/efdf3513-2235-4aee-a59f-28dce7053f6e/data", data = inf, headers=headers)
 print(r)
 print(r.content)
-'''
 
-temp = 22
-data = datetime.datetime.utcnow().isoformat()
-payload = '{"data": {"environment_monitoring": [{"teste": %d,"timestamp": "%s"}]}}' %(temp, data)
-print (payload)
