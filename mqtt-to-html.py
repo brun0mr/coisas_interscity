e9a8de7a060811a05fca0e5a97c533a454090708
brun0mr <bruno.rotondaro@unifesp.br>
2020-05-23 17:24:07 -0300

    mqtt-to-html funcionando sem integrecao com lora

--- mqtt-to-html.py
+++ mqtt-to-html.py
@@ -6,19 +6,20 @@ import json
 
 
 def on_message(client, userdata, message):
+    headers = {'content-Type': 'application/json'}
     print("salvando mensagem...")
     temp = str(message.payload.decode("utf-8"))
     data = datetime.datetime.utcnow().isoformat()
     print("a mensagem recebida foi :" + temp)
     print("Mandando para o interscity...")
     payload = '{"data": {"environment_monitoring": [{"teste": %s,"timestamp": "%s"}]}}' %(temp, data)
-    r = requests.post("http://34.95.169.45:8000/adaptor/resources/efdf3513-2235-4aee-a59f-28dce7053f6e/data", data = json.dumps(y), headers=headers)
+    r = requests.post("http://34.95.169.45:8000/adaptor/resources/efdf3513-2235-4aee-a59f-28dce7053f6e/data", data = payload, headers=headers)
     print("postado no interscity")
     print(r)
     print(r.content)
 
 subscribe.callback(on_message, "teste", hostname = "134.122.122.88", port=1883 )
-
+print("listening...")
         
        
 # auth = {‘username’:”<bruno>”, ‘password’:”<bruno>”}
